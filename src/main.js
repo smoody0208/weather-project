7f9f8f40b5f4546bb14588b1e64533111a77cbc4
Spencer Moody <spencer.moody@outlook.com>
2020-07-01 15:49:12 -0700

    Update main.js and index.html
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,37 @@ import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+function showElements(response) {
+  $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+  $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  $('.showErrors').text("");
+}
+
+function showError(error) {
+  $('.showErrors').text(`There was an issue: ${error}.`);
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    async function asyncApiCall() {
+      try {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`);
+        if (!response.ok) {
+          showError(response.statusText);
+        } else {
+          const jsonifiedResponse = await response.json();
+          showElements(jsonifiedResponse);
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    }
+    asyncApiCall();
+  });
+});
